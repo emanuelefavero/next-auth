7c7dfeb514c254d7a9eae4087eb0b76ff1d1148b
emanuelefavero <emafaux89@gmail.com>
2023-02-05 03:29:58 +0100

    first commit

---
+++ pages/api/auth/[...nextauth].js
@@ -0,0 +1,17 @@
+import NextAuth from 'next-auth'
+import GithubProvider from 'next-auth/providers/github'
+
+export const authOptions = {
+  // Configure one or more authentication providers
+  providers: [
+    GithubProvider({
+      clientId: process.env.GITHUB_ID,
+      clientSecret: process.env.GITHUB_SECRET,
+    }),
+    // ...add more providers here
+  ],
+
+  // A database is optional, but required to persist accounts in a database
+  // database: process.env.DATABASE_URL,
+}
+export default NextAuth(authOptions)
